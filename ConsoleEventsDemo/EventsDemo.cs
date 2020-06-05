b3ca8862bd2d7689208eabbbc6003bf576499698
ramakrishnamb <ramakrishnamb@gmail.com>
2020-06-05 20:31:13 +0530

    Add project files.

---
+++ ConsoleEventsDemo/EventsDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleEventsDemo
+{
+    public class EventsDemo
+    {
+        public delegate void delEventWrapper(string msg);
+
+        //step 1 : Create event
+        public event delEventWrapper evntWrapperforDelegate;
+        public event EventHandler<string> evntEventHandler;
+
+        public void OnDemoeventsfordelegatewrapper()
+        {
+            //step 2 : invoking event -- Publisher
+            evntWrapperforDelegate?.Invoke("This message from event argument which is using delegate as wrapper.");
+        }
+    }
+}
---
+++ ConsoleEventsDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleEventsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventsDemo eventsDemo = new EventsDemo();
+            //step 3 : consume event -- Subscriber
+            eventsDemo.evntWrapperforDelegate += EventsDemo_evntWrapperforDelegate;
+
+            eventsDemo.OnDemoeventsfordelegatewrapper();
+            // Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        private static void EventsDemo_evntWrapperforDelegate(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
